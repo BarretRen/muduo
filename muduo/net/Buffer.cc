d405ee0ed047abf112601399a6bcf02fe5268cdd
Barret Ren <barret.ren@outlook.com>
2020-12-04 09:14:09 +0000

    comments for buffer readFd

--- muduo/net/Buffer.cc
+++ muduo/net/Buffer.cc
@@ -25,17 +25,18 @@ const size_t Buffer::kInitialSize;
 ssize_t Buffer::readFd(int fd, int* savedErrno)
 {
   // saved an ioctl()/FIONREAD call to tell how much to read
-  char extrabuf[65536];
-  struct iovec vec[2];
+  char extrabuf[65536];//在栈上定义65536的临时buffer
+  struct iovec vec[2];//定义两个iovec
   const size_t writable = writableBytes();
-  vec[0].iov_base = begin()+writerIndex_;
+  vec[0].iov_base = begin()+writerIndex_;//第一块指向Buffer类的writable区域
   vec[0].iov_len = writable;
-  vec[1].iov_base = extrabuf;
+  vec[1].iov_base = extrabuf;//第二块指向临时buffer
   vec[1].iov_len = sizeof extrabuf;
   // when there is enough space in this buffer, don't read into extrabuf.
   // when extrabuf is used, we read 128k-1 bytes at most.
+  //优先从socket读数据到Buffer类的writable区域，如果数据量太大，则多出来的数据写到临时buffer
   const int iovcnt = (writable < sizeof extrabuf) ? 2 : 1;
-  const ssize_t n = sockets::readv(fd, vec, iovcnt);
+  const ssize_t n = sockets::readv(fd, vec, iovcnt);//只需要一次系统调用，减少了开销
   if (n < 0)
   {
     *savedErrno = errno;
@@ -47,6 +48,7 @@ ssize_t Buffer::readFd(int fd, int* savedErrno)
   else
   {
     writerIndex_ = buffer_.size();
+    //将临时buffer的数据写入Buffer类的区域，这里vector动态增加空间，所以初始化时节省了内存
     append(extrabuf, n - writable);
   }
   // if (n == writable + sizeof extrabuf)
